49a158b7a36be1e047657e65d23e9abeb8a69d93
AlisterTT <gengxin5213@gmail.com>
2022-11-09 12:59:12 +0800

    Update comicsc.py
    get redirect url

--- comicsc.py
+++ comicsc.py
@@ -7,7 +7,8 @@ for i in range(len(import_list)):
     choose_list = str(i+1) + '. ' + a
     print(choose_list)
 choose = int(input('选择漫画序号：'))
-importurl = import_list[choose-1]['url']
+import_tmp = import_list[choose-1]['url']
+importurl = requests.get(import_tmp).url
 n = re.search(r'comic\/(.*?)$',importurl)
 comic_name = n.group(1)
 m = getfunction.getlist(importurl)
