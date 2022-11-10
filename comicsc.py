9c3b0de3d900a65f624226876b2865d134acc6f5
AlisterTT <gengxin5213@gmail.com>
2022-11-10 15:50:11 +0800

    Update comicsc.py
    计划对其他网站兼容

--- comicsc.py
+++ comicsc.py
@@ -1,4 +1,4 @@
-import requests,re,time,os,ast,getfunction
+import requests,time,os,ast,getfunction,comic_site
 
 with open('comic_list.txt', 'r',encoding='UTF-8') as f:
     import_list = ast.literal_eval(f.read())
@@ -9,28 +9,15 @@ for i in range(len(import_list)):
 choose = int(input('选择漫画序号：'))
 import_tmp = import_list[choose-1]['url']
 importurl = requests.get(import_tmp).url
-n = re.search(r'comic\/(.*?)$',importurl)
-comic_name = n.group(1)
-m = getfunction.getlist(importurl)
-for i in range(len(m)):
-    print (f'{i+1}. {m[i][1]}')
-comic_capter = int(input('输入章节序号：'))
-print('请稍等...')
-comic_capter = comic_capter - 1
-baseurl = f'https://cn.webmota.com/comic/chapter/{comic_name}/0_{m[comic_capter][0]}_'
-directory_name = import_list[choose-1]['name']+'-'+m[comic_capter][1]
 
-comic_url = []
-temlist = []
-for i in range(1,10):
-    comic_url.append(baseurl+str(i)+'.html')
-for i in range(len(comic_url)):
-    html_str = requests.get(comic_url[i]).content.decode()
-    img_list = re.findall('img src="(.*?)" alt',html_str, re.S)
-    for u in range(len(img_list)):
-        temlist.append(img_list[u])
-final_list = sorted(set(temlist),key=temlist.index)
+if 'baozimh' in import_tmp:
+    n = comic_site.baozimh.output_list(importurl,import_list,choose)
+else:
+    print('你添加的网站不在支持列表内')
+    exit()
 
+final_list = n[0]
+directory_name = n[1]
 output_dir = ['PDF', directory_name]
 for i in range(len(output_dir)):
     if not os.path.exists(output_dir[i]):
@@ -43,7 +30,6 @@ for i in range (len(final_list)):
     time.sleep(0.1)
 
 folder = f'./{directory_name}/'
-#pdfFile = f'./{directory_name}/{directory_name}_final.pdf'
 pdfFile = f'./PDF/{directory_name}.pdf'
 getfunction.combine_imgs_pdf(folder, pdfFile)
 print('\nComplate')
